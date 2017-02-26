17285c87b6552e3f50cf4f57563528e4131f7a2f
Daniel Stonier <d.stonier@gmail.com>
2017-02-26 23:35:59 +0900

    [mock] generic action server

--- py_trees_ros/mock/action_server.py
+++ py_trees_ros/mock/action_server.py
@@ -1,5 +1,89 @@
-'''
-Created on 26Feb.,2017
+#!/usr/bin/env python
+#
+# License: BSD
+#   https://raw.githubusercontent.com/stonier/py_trees/devel/LICENSE
+#
+##############################################################################
+# Documentation
+##############################################################################
 
-@author: snorri
-'''
+"""
+Mocks the move base action server of the ROS navigation stack.
+"""
+
+##############################################################################
+# Imports
+##############################################################################
+
+import actionlib
+import py_trees_ros
+import rospy
+
+##############################################################################
+# Classes
+##############################################################################
+
+
+class ActionServer(object):
+    """
+    Generic action server that can be subclassed to quickly create action
+    servers of varying types in a mock simulation.
+
+    Args:
+        action_name (:obj:`str`): name of the action server (e.g. move_base)
+        action_type (:obj:`any`): type of the action server (e.g. move_base_msgs.msg.MoveBaseAction
+        worker (:obj:`func`): callback to be executed inside the execute loop, no args
+        duration (:obj:`int`): time for a goal to complete (seconds)
+    """
+    def __init__(self, action_name, action_type, worker, duration=5):
+        self.duration = duration
+        self.worker = worker
+        self.action_server = actionlib.SimpleActionServer(action_name,
+                                                          action_type,
+                                                          execute_cb=self.execute,
+                                                          auto_start=False
+                                                          )
+        self.percent_completed = 0
+        self.title = action_name.replace('_', ' ').upper()
+        self.action = action_type()
+
+    def start(self):
+        """
+        Start the action server.
+        """
+        self.action_server.start()
+
+    def execute(self, goal):
+        """
+        Check for pre-emption, but otherwise just spin around gradually incrementing
+        a hypothetical 'percent' done.
+
+        Args:
+            goal (:obj:`any`): goal of type specified by the action_type in the constructor.
+        """
+        # goal.target_pose = don't care
+        frequency = 3.0  # hz
+        increment = 100 / (frequency * self.duration)
+        self.percent_completed = 0
+        rate = rospy.Rate(frequency)  # hz
+        rospy.loginfo("{title}: received a goal".format(title=self.title))
+        # if we just received a goal, we erase any previous pre-emption
+        self.action_server.preempt_request = False
+        while True:
+            if rospy.is_shutdown() or self.action_server.is_preempt_requested():
+                rospy.loginfo("{title}: goal preempted".format(title=self.title))
+                self.action_server.set_preempted(self.action.action_result, "goal was preempted")
+                success = False
+                break
+            if self.percent_completed >= 100:
+                rospy.loginfo("{title}: feedback 100%".format(title=self.title))
+                success = True
+                break
+            else:
+                rospy.loginfo("{title}: feedback {percent:.2f}%".format(title=self.title, percent=self.percent_completed))
+                self.percent_completed += increment
+                self.worker()
+            rate.sleep()
+        if success:
+            rospy.loginfo("{title}: goal success".format(title=self.title))
+            self.action_server.set_succeeded(self.action.action_result, "goal reached")
--- py_trees_ros/mock/move_base.py
+++ py_trees_ros/mock/move_base.py
@@ -15,19 +15,20 @@ Mocks the move base action server of the ROS navigation stack.
 # Imports
 ##############################################################################
 
-import actionlib
 import geometry_msgs.msg as geometry_msgs
 import move_base_msgs.msg as move_base_msgs
 import nav_msgs.msg as nav_msgs
 import py_trees_ros
 import rospy
 
+from . import action_server
+
 ##############################################################################
 # Classes
 ##############################################################################
 
 
-class MoveBase(object):
+class MoveBase(action_server.ActionServer):
     """
     Simulates:
 
@@ -38,11 +39,15 @@ class MoveBase(object):
     Args:
         odometry_topic (:obj:`str`): name of the odometry topic
         pose_topic (:obj:`str`): name of the pose (with covariance stamped) topic
-        control_duration (:obj:`int`): time for a goal to complete (seconds)
+        duration (:obj:`int`): time for a goal to complete (seconds)
     """
-    def __init__(self, odometry_topic='/odom', pose_topic='/pose', control_duration=5):
-        # rospy.get_param("~control_duration", 5)  # secs : time for a goal to complete
-        self.control_duration = control_duration
+    def __init__(self, odometry_topic='/odom', pose_topic='/pose', duration=5):
+        super(MoveBase, self).__init__(action_name="move_base",
+                                       action_type=move_base_msgs.MoveBaseAction,
+                                       worker=self.worker,
+                                       duration=duration
+                                       )
+
         self.odometry = nav_msgs.Odometry()
         self.odometry.pose.pose.position = geometry_msgs.Point(0, 0, 0)
         self.pose = geometry_msgs.PoseWithCovarianceStamped()
@@ -57,17 +62,14 @@ class MoveBase(object):
             ]
         )
 
-        self.action_server = actionlib.SimpleActionServer('move_base',
-                                                          move_base_msgs.MoveBaseAction,
-                                                          execute_cb=self.execute,
-                                                          auto_start=False
-                                                          )
-        self.percent_completed = 0
-        self.result = move_base_msgs.MoveBaseResult()
         self.publishers.pose.publish(self.pose)
         self.publishers.odometry.publish(self.odometry)
-        self.action_server.start()
         self.publishing_timer = rospy.Timer(period=rospy.Duration(0.5), callback=self.publish, oneshot=False)
+        self.start()
+
+    def worker(self):
+        self.odometry.pose.pose.position.x += 0.01
+        self.pose.pose.pose.position.x += 0.01
 
     def publish(self, unused_event):
         """
@@ -76,32 +78,3 @@ class MoveBase(object):
         """
         self.publishers.odometry.publish(self.odometry)
         self.publishers.pose.publish(self.pose)
-
-    def execute(self, goal):
-        # goal.target_pose = don't care
-        frequency = 3.0  # hz
-        increment = 100 / (frequency * self.control_duration)
-        self.percent_completed = 0
-        rate = rospy.Rate(frequency)  # hz
-        rospy.loginfo("Move Base: received a goal")
-        # if we just received a goal, we erase any previous pre-emption
-        self.action_server.preempt_request = False
-        while True:
-            if rospy.is_shutdown() or self.action_server.is_preempt_requested():
-                rospy.loginfo("Move Base: goal preempted")
-                self.action_server.set_preempted(self.result, "goal was preempted")
-                success = False
-                break
-            if self.percent_completed >= 100:
-                rospy.loginfo("Move Base: feedback 100%")
-                success = True
-                break
-            else:
-                rospy.loginfo("Move Base: feedback {0:.2f}%".format(self.percent_completed))
-                self.percent_completed += increment
-                self.odometry.pose.pose.position.x += 0.01
-                self.pose.pose.pose.position.x += 0.01
-            rate.sleep()
-        if success:
-            rospy.loginfo("Move Base: goal success")
-            self.action_server.set_succeeded(self.result, "goal reached")
