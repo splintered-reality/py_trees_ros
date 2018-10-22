b930cc2749a9a3442a6a002fb40b74a907de6f70
Daniel Stonier <d.stonier@gmail.com>
2018-10-22 06:59:08 -0400

    [infra] setup.cfg and setup.py updates

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [ # ] if os.environ.get('AMENT_PREFIX_PATH') else [
 
 ]
 
-d = setup(
+setup(
     name='py_trees_ros',
     version='0.5.14',  # also update package.xml and version.py
     packages=find_packages(exclude=['tests*', 'docs*', 'launch*']),
@@ -27,14 +27,20 @@ d = setup(
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Software Development :: Libraries'
     ],
-    description="ROS extensions for py-trees, a pythonic implementation of behaviour trees",
-    long_description="ROS specific behaviours, trees, logging and tutorials for py-trees.",
+    description=(
+        "ROS extensions for py-trees, a pythonic implementation of "
+        "behaviour trees."
+    ),
+    long_description=(
+        "ROS extensions for py-trees, a pythonic implementation of "
+        "behaviour trees. It includes ROS specific behaviours a tree"
+        "manager with ROS communication handles for debugging and"
+        "visualisation, logging and various tutorials."
+    ),
     license='BSD',
     # test_suite = 'nose.collector',
     # tests_require=['nose', 'pytest', 'flake8', 'yanc', 'nose-htmloutput']
     # tests_require=['pytest'],
-    # global installs, for package relative installs, look in CMakeLists.txt
-    # scripts=['scripts/demo_tree'],
     entry_points={
          'console_scripts': [
              'py-trees-blackboard-watcher = py_trees_ros.programs.blackboard_watcher:main',
