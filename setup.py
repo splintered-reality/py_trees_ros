418ec69d256d5bd846b91b7a45c7762580f554fc
Daniel Stonier <d.stonier@gmail.com>
2019-04-01 14:35:16 -0400

    even simpler setup

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 
-import os
-
 from setuptools import find_packages, setup
 
-package_name = 'py_trees_ros'
-
 setup(
-    name=package_name,
-    version='0.5.14',  # also update package.xml and version.py
+    name='py_trees_ros',
+    version='0.5.14',
     packages=find_packages(exclude=['docs*']),
     author='Daniel Stonier, Naveed Usmani, Michal Staniaszek',
     maintainer='Daniel Stonier <d.stonier@gmail.com>',
@@ -22,15 +18,7 @@ setup(
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Software Development :: Libraries'
     ],
-    description=(
-        "ROS extensions for py-trees, a pythonic implementation of "
-        "behaviour trees."
-    ),
-    long_description=(
-        "ROS extensions for py-trees, a pythonic implementation of "
-        "behaviour trees. It includes ROS specific behaviours a tree"
-        "manager with ROS communication handles for debugging and"
-        "visualisation, logging and various tutorials."
-    ),
+    description=("Short"),
+    long_description=("Long"),
     license='BSD',
 )
