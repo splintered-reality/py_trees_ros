d2e32d9357decb98623571597a92334207126ba5
Daniel Stonier <d.stonier@gmail.com>
2019-08-11 00:21:58 -0400

    1.1.2

--- doc/conf.py
+++ doc/conf.py
@@ -46,9 +46,9 @@ copyright = u'2019, Daniel Stonier'
 # built documents.
 #
 # The short X.Y version.
-version = "1.0"
+version = "1.1"
 # The full version, including alpha/beta/rc tags.
-release = "1.0.0"
+release = "1.1.2"
 
 ##############################################################################
 # Regular Sphinx Configuration
