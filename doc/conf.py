76e2801306e27739aa023f25a76fc1d8e9e4d649
Daniel Stonier <d.stonier@gmail.com>
2020-03-06 19:19:59 -0500

    2.0.10

--- doc/conf.py
+++ doc/conf.py
@@ -48,7 +48,7 @@ copyright = u'2020, Daniel Stonier'
 # The short X.Y version.
 version = "2.0"
 # The full version, including alpha/beta/rc tags.
-release = "2.0.9"
+release = "2.0.10"
 
 ##############################################################################
 # Regular Sphinx Configuration
